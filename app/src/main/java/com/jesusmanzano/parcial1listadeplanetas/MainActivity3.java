8bf23e2a34d7338deddcef113ffd65f9a2630034
JesusManzano24 <9991404825j@gmail.com>
2024-06-10 16:29:54 -0600

    Segundo Commit

--- app/src/main/java/com/jesusmanzano/parcial1listadeplanetas/MainActivity3.java
+++ app/src/main/java/com/jesusmanzano/parcial1listadeplanetas/MainActivity3.java
@@ -1,6 +1,9 @@
 package com.jesusmanzano.parcial1listadeplanetas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,7 +35,6 @@ public class MainActivity3 extends AppCompatActivity {
         }
     }
 
-    // Array con la información de los planetas
     PlanetaInfo[] planetas = {
             new PlanetaInfo(
                     "Mercurio",
@@ -109,6 +111,7 @@ public class MainActivity3 extends AppCompatActivity {
 
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,9 +120,17 @@ public class MainActivity3 extends AppCompatActivity {
         // Recuperar la posición del planeta seleccionado
         int posicionSeleccionada = getIntent().getIntExtra("POSICION_PLANETA", 0);
         actualizarInformacionDelPlaneta(posicionSeleccionada);
+
+        ImageButton botonIrAOtraPagina = (ImageButton) findViewById(R.id.botonAtras);
+        botonIrAOtraPagina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    // Método para actualizar la UI con la información del planeta seleccionado
     public void actualizarInformacionDelPlaneta(int posicionSeleccionada) {
         PlanetaInfo planetaSeleccionado = planetas[posicionSeleccionada];
 
